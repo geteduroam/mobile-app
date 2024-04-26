60a1c6544636644a36b14766b5232e21d7c0733d
Dániel Zolnai <daniel@egeniq.com>
2024-04-26 13:01:24 +0200

    GETEDUROAM-0: Remove unused shared code

--- shared/core/src/commonMain/kotlin/app/eduroam/shared/models/ViewModel.kt
+++
@@ -1,8 +0,0 @@
-package app.eduroam.shared.models
-
-import kotlinx.coroutines.CoroutineScope
-
-expect abstract class ViewModel() {
-    val viewModelScope: CoroutineScope
-    protected open fun onCleared()
-}
